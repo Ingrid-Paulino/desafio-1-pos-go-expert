c403630527282ab4fbb108dd18d92eedb67319fb
IngridPaulino <ingrid.paulino@mercadolivre.com>
2024-08-18 11:17:00 -0300

    refactor code

--- client/client.go
+++ client/client.go
@@ -7,24 +7,22 @@ import (
 	"net/http"
 	"os"
 	"time"
-
-	"gorm.io/gorm"
 )
 
-type Exchange struct {
-	Code       string `json:"code"`
-	Codein     string `json:"codein"`
-	Name       string `json:"name"`
-	High       string `json:"high"`
-	Low        string `json:"low"`
-	VarBid     string `json:"varBid"`
-	PctChange  string `json:"pctChange"`
-	Bid        string `json:"bid"`
-	Ask        string `json:"ask"`
-	Timestamp  string `json:"timestamp"`
-	CreateDate string `json:"create_date"`
-	gorm.Model
-}
+//type Exchange struct {
+//	Code       string `json:"code"`
+//	Codein     string `json:"codein"`
+//	Name       string `json:"name"`
+//	High       string `json:"high"`
+//	Low        string `json:"low"`
+//	VarBid     string `json:"varBid"`
+//	PctChange  string `json:"pctChange"`
+//	Bid        string `json:"bid"`
+//	Ask        string `json:"ask"`
+//	Timestamp  string `json:"timestamp"`
+//	CreateDate string `json:"create_date"`
+//	gorm.Model
+//}
 
 func main() {
 	ctx := context.Background()
